d2231900b0b3a9c597a04786a72ff7201ce987a8
Ezuzu <ezuzufavour@gmail.com>
2024-07-27 11:54:11 +0100

    smile

--- randomization.js
+++ randomization.js
@@ -14,6 +14,10 @@ loveScore = Math.floor(loveScore) + 1; //add 1 to make it a range of 1 to 100
 
 if (loveScore >= 70) {
     console.log( "Your love score is "+ loveScore+" Your love is like kanye love for kanye");
-}else {
-    console.log("Your love score is "+ loveScore +"%");
+}
+if (loveScore > 30 && loveScore<= 70 ) {
+    console.log("Your love score is "+ loveScore+ "%")
+}
+if (loveScore <=30) {
+    console.log("Your lovescore is "+ loveScore + "%" + " You go together like oil and water!")
 }
