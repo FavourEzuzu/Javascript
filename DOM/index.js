7e3a0f00ae8059d8bf189e826f88c1b6936b9f38
Ezuzu <ezuzufavour@gmail.com>
2024-07-27 23:43:35 +0100

    js

---
+++ DOM/index.js
@@ -0,0 +1,12 @@
+document.getElementById("#list")
+
+// query selector is used to select any type of element individually
+document.querySelector("#list a").style.color = "magenta"
+
+// while the query select all is used to select all the element with the class or id specified
+
+
+// we can separate the html, css and js into different files and folders
+// this is called separation of concerns
+// it makes the code more readable and easier to maintain
+document.querySelector("h1").classList.add("huge");
